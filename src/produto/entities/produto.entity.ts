9dbc19d5aa26c9b1377d4837616c5849479f9486
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-10-18 10:18:31 -0300

    Add files via upload

---
+++ src/produto/entities/produto.entity.ts
@@ -0,0 +1,24 @@
+import { IsNotEmpty } from "class-validator";
+import { Categoria } from "src/categoria/entities/categoria.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: "tb_produto"})
+    export class Produto{
+
+        @PrimaryGeneratedColumn()
+        id: number
+        
+        @IsNotEmpty()
+        @Column({length: 100, nullable: false})
+        titulo: string
+
+        @IsNotEmpty()
+        @Column({length: 100, nullable: false})
+        editora: string
+
+        @Column({length: 100, nullable: true})
+        nota: string
+
+        @ManyToOne (() => Categoria, (Categoria) => Categoria.produto)
+        categoria: Categoria []
+}
